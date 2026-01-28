3e7a8032d18730b52b42b72cf9e5db6557938f1d
Abdellah Sheikh <133889483+aaas72@users.noreply.github.com>
2026-01-28 17:59:58 +0300

    Add RequestLoggingMiddleware for request logging

--- Middlewares/RequestLoggingMiddleware.cs
+++ Middlewares/RequestLoggingMiddleware.cs
@@ -1,1 +1,18 @@
 
+namespace UserManagementApi.Middlewares;
+
+public class RequestLoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public RequestLoggingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path}");
+        await _next(context);
+    }
+}
