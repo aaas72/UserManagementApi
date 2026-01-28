2aef3235b523e720925abb1313b9d409957bbbab
Abdellah Sheikh <133889483+aaas72@users.noreply.github.com>
2026-01-28 17:59:28 +0300

    Add AppDbContext for User management

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,1 +1,15 @@
 
+using Microsoft.EntityFrameworkCore;
+using UserManagementApi.Models;
+
+namespace UserManagementApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+}
